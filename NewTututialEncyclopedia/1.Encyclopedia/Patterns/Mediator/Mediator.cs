868e79070a5606b04490c2311deb71491cbcaa83
Paweł <paweljakubczyk1@gmail.com>
2024-07-17 11:47:12 +0200

    Change Nmes Reaorganize files

---
+++ NewTututialEncyclopedia/1.Encyclopedia/Patterns/Mediator/Mediator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTututial._1.Encyclopedia.Patterns.Mediator
+{
+    internal class Mediator
+    {
+    }
+}
