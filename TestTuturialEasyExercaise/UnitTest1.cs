e72967e8a174b0b952dff7a9189405a4bd285865
Paweł <85743550+PawelJakubczyk@users.noreply.github.com>
2024-07-24 18:41:24 +0200

    Change project names + resegregate

---
+++ TestTuturialEasyExercaise/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestTuturialEasyExercaise;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TestTuturialMovieApi/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestTuturialMovieApi;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
